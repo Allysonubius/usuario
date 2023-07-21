339bf9e3f0aa82033ed5f945d0b681b1412e668a
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2023-07-21 13:40:46 -0300

    Refatoração do codigo para cobertura do sonar

--- src/test/java/com/backend/usuario/domain/request/user/UserRequestTest.java
+++
@@ -1,9 +0,0 @@
-package com.backend.usuario.domain.request.user;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = UserCreateUserRequest.class)
-class UserRequestTest {
-
-}
